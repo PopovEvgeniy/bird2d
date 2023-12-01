a30bfc40218878466cdb0692edc8069658365a0f
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-12-01 21:22:38 +0300

    Image resizer back-ported from Eugene game development kit 7.7

--- bird2d.cpp
+++ bird2d.cpp
@@ -865,6 +865,8 @@ namespace BIRD2D
    image.set_length(0);
    source_width=0;
    source_height=0;
+   x_ratio=0;
+   y_ratio=0;
    target_width=1;
    target_height=1;
   }
@@ -881,12 +883,12 @@ namespace BIRD2D
 
   unsigned int Resizer::get_source_x(const unsigned int x) const
   {
-   return (x*(source_width-1)+1)/(target_width-1);
+   return (x*x_ratio+1)/UCHAR_MAX;
   }
 
   unsigned int Resizer::get_source_y(const unsigned int y) const
   {
-   return (y*(source_height-1)+1)/(target_height-1);
+   return (y*y_ratio+1)/UCHAR_MAX;
   }
 
   unsigned int Resizer::get_next_x(const unsigned int x) const
@@ -1008,6 +1010,12 @@ namespace BIRD2D
 
   }
 
+  void Resizer::calculate_ratio()
+  {
+   x_ratio=(source_width*UCHAR_MAX)/target_width;
+   y_ratio=(source_height*UCHAR_MAX)/target_height;
+  }
+
   void Resizer::calculate_size()
   {
    while (target_width<source_width)
@@ -1034,6 +1042,7 @@ namespace BIRD2D
    this->set_setting(width,height);
    this->calculate_size();
    this->correct_size(limit);
+   this->calculate_ratio();
    this->create_texture();
    this->resize_image(target);
   }
