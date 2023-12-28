4fd198f2e19efff56203f66bc31e806f1e95af77
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-12-29 00:07:08 +0300

    Isometric level subsystem back-ported from Eugene game development kit 7.8.4

--- bird2d.cpp
+++ bird2d.cpp
@@ -3666,6 +3666,8 @@ namespace BIRD2D
 
   World::World()
   {
+   surface_width=1;
+   surface_height=1;
    half_tile_width=1;
    half_tile_height=1;
   }
@@ -3675,7 +3677,7 @@ namespace BIRD2D
 
   }
 
-  void World::initialize(const int tile_width,const int tile_height)
+  void World::initialize(const int tile_width,const int tile_height,const int screen_width,const int screen_height)
   {
    if (tile_width>1)
    {
@@ -3684,6 +3686,14 @@ namespace BIRD2D
    if (tile_height>1)
    {
     half_tile_height=tile_height/2;
+   }
+   if (screen_width>1)
+   {
+    surface_width=screen_width;
+   }
+   if (screen_height>1)
+   {
+    surface_height=screen_height;
    }
 
   }
@@ -3718,6 +3728,33 @@ namespace BIRD2D
     column/=2;
    }
    return column;
+  }
+
+  int World::get_row_amount() const
+  {
+   int amount;
+   amount=surface_width/half_tile_width;
+   if ((surface_width%half_tile_width)!=0)
+   {
+    ++amount;
+   }
+   return amount;
+  }
+
+  int World::get_column_amount() const
+  {
+   int amount;
+   amount=surface_height/half_tile_height;
+   if ((surface_height%half_tile_height)!=0)
+   {
+    ++amount;
+   }
+   return amount;
+  }
+
+  int World::get_tile_amount() const
+  {
+   return this->get_row_amount()*this->get_column_amount();
   }
 
  }
