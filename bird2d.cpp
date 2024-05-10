205f603c32c3ca0be049d902eccda6e930d2b4b2
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-05-10 17:51:48 +0700

    Source code synced with Eugene game development kit 8.6.8

--- bird2d.cpp
+++ bird2d.cpp
@@ -1811,16 +1811,6 @@ namespace BIRD2D
 
   }
 
-  FILE *Binary_File::get_target()
-  {
-   return target;
-  }
-
-  void Binary_File::set_target(FILE *point)
-  {
-   target=point;
-  }
-
   void Binary_File::close()
   {
    if (target!=NULL)
@@ -1840,17 +1830,6 @@ namespace BIRD2D
 
   }
 
-  long int Binary_File::get_position()
-  {
-   long int position;
-   position=0;
-   if (target!=NULL)
-   {
-    position=ftell(target);
-   }
-   return position;
-  }
-
   long int Binary_File::get_length()
   {
    long int length;
@@ -1864,15 +1843,14 @@ namespace BIRD2D
    return length;
   }
 
+  long int Binary_File::get_position()
+  {
+   return (target==NULL) ? 0:ftell(target);
+  }
+
   bool Binary_File::check_error()
   {
-   int error;
-   error=0;
-   if (target!=NULL)
-   {
-    error=ferror(target);
-   }
-   return error!=0;
+   return (target==NULL) ? true:(ferror(target)!=0);
   }
 
   bool Binary_File::is_open() const
@@ -1898,16 +1876,16 @@ namespace BIRD2D
   void Input_File::open(const char *name)
   {
    this->close();
-   this->set_target(fopen(name,"rb"));
+   target=fopen(name,"rb");
   }
 
   void Input_File::read(void *buffer,const size_t length)
   {
-   if (this->get_target()!=NULL)
+   if (this->target!=NULL)
    {
     if (buffer!=NULL)
     {
-     fread(buffer,sizeof(char),length,this->get_target());
+     fread(buffer,sizeof(char),length,target);
     }
 
    }
@@ -1932,22 +1910,22 @@ namespace BIRD2D
   void Output_File::open(const char *name)
   {
    this->close();
-   this->set_target(fopen(name,"wb"));
+   target=fopen(name,"wb");
   }
 
   void Output_File::create_temp()
   {
    this->close();
-   this->set_target(tmpfile());
+   target=tmpfile();
   }
 
   void Output_File::write(const void *buffer,const size_t length)
   {
-   if (this->get_target()!=NULL)
+   if (this->target!=NULL)
    {
     if (buffer!=NULL)
     {
-     fwrite(buffer,sizeof(char),length,this->get_target());
+     fwrite(buffer,sizeof(char),length,target);
     }
 
    }
@@ -1956,9 +1934,9 @@ namespace BIRD2D
 
   void Output_File::flush()
   {
-   if (this->get_target()!=NULL)
+   if (target!=NULL)
    {
-    fflush(this->get_target());
+    fflush(target);
    }
 
   }
@@ -1968,10 +1946,10 @@ namespace BIRD2D
  namespace Misc
  {
 
-    Memory::Memory()
-    {
-     memset(&information,0,sizeof(struct sysinfo));
-    }
+  Memory::Memory()
+  {
+   memset(&information,0,sizeof(struct sysinfo));
+  }
 
    Memory::~Memory()
    {
