80da9fbc391f223cb10b6252c84f4e7b08ae1341
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-05-24 20:50:45 +0700

    The source code synced with the Black game development kit 7.3.9

--- bird2d.cpp
+++ bird2d.cpp
@@ -2357,19 +2357,9 @@ namespace BIRD2D
    highest_y_offset=((screen_height-viewport_height)*screen_height)/viewport_height;
   }
 
-  bool Camera::check_viewport_width(const unsigned int width) const
-  {
-   return (width>0) && (width<=screen_width);
-  }
-
-  bool Camera::check_viewport_heigth(const unsigned int height) const
-  {
-   return (height>0) && (height<=screen_height);
-  }
-
   void Camera::set_viewport_width(const unsigned int width)
   {
-   if (this->check_viewport_width(width)==true)
+   if ((width>0) && (width<=screen_width))
    {
     viewport_width=width;
    }
@@ -2382,7 +2372,7 @@ namespace BIRD2D
 
   void Camera::set_viewport_heigth(const unsigned int height)
   {
-   if (this->check_viewport_heigth(height)==true)
+   if ((height>0) && (height<=screen_height))
    {
     viewport_height=height;
    }
