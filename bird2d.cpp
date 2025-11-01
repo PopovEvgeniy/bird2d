2ad2d31285c6e6a22937b9869cf7ba551515eb0e
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-02 02:26:25 +0700

    The source code synced with the Eugene Game Development Kit 9.8.1

--- bird2d.cpp
+++ bird2d.cpp
@@ -1273,6 +1273,7 @@ namespace BIRD2D
    if (glIsEnabled(GL_ALPHA_TEST)==GL_FALSE)
    {
     glEnable(GL_ALPHA_TEST);
+    glEnable(GL_BLEND);
    }
 
   }
@@ -1282,6 +1283,7 @@ namespace BIRD2D
    if (glIsEnabled(GL_ALPHA_TEST)==GL_TRUE)
    {
     glDisable(GL_ALPHA_TEST);
+    glDisable(GL_BLEND);
    }
 
   }
@@ -1358,7 +1360,6 @@ namespace BIRD2D
   void Render::set_perfomance_settings()
   {
    glDisable(GL_TEXTURE_1D);
-   glDisable(GL_BLEND);
    glDisable(GL_TEXTURE_GEN_Q);
    glDisable(GL_TEXTURE_GEN_R);
    glDisable(GL_TEXTURE_GEN_S);
@@ -1401,6 +1402,7 @@ namespace BIRD2D
    glDisable(GL_MAP2_VERTEX_3);
    glDisable(GL_MAP2_VERTEX_4);
    glEnable(GL_TEXTURE_2D);
+   glEnable(GL_BLEND);
    glEnable(GL_ALPHA_TEST);
    glEnableClientState(GL_VERTEX_ARRAY);
    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -1424,6 +1426,7 @@ namespace BIRD2D
    glDrawBuffer(GL_BACK);
    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
    glShadeModel(GL_FLAT);
+   glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.6f);
    glClearColor(0.0,0.0,0.0,0.0);
   }
@@ -1456,8 +1459,8 @@ namespace BIRD2D
   {
    this->set_image_settings();
    this->set_perspective(width,height);
-   this->set_perfomance_settings();
    this->set_render_hints();
+   this->set_perfomance_settings();
    this->set_common_settings();
    this->set_matrix_settings();
    this->disable_depth_buffer();
@@ -3220,6 +3223,21 @@ namespace BIRD2D
   void Billboard::complex_mirror()
   {
    mirror=Core::MIRROR_BOTH;
+  }
+
+  bool Billboard::is_horizontally_mirrored() const
+  {
+   return mirror==Core::HORIZONTAL_MIRROR;
+  }
+
+  bool Billboard::is_vertically_mirrored() const
+  {
+   return mirror==Core::VERTICAL_MIRROR;
+  }
+
+  bool Billboard::is_complex_mirrored() const
+  {
+   return mirror==Core::MIRROR_BOTH;
   }
 
   void Billboard::go_start()
