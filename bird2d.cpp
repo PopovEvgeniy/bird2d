283978d5559a81dc188fe5f078ed69a5b3759424
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-12-26 21:25:07 +0700

    The source code synced with the Eugene Game Development Kit 10.1.2

--- bird2d.cpp
+++ bird2d.cpp
@@ -4555,15 +4555,10 @@ namespace BIRD2D
   unsigned int Tilemap::get_row_amount(const unsigned int viewport_width) const
   {
    unsigned int amount;
-   amount=0;
-   if (viewport_width>0)
+   amount=viewport_width/cell_width;
+   if ((viewport_width%cell_width)!=0)
    {
-    amount=viewport_width/cell_width;
-    if ((viewport_width%cell_width)!=0)
-    {
-     ++amount;
-    }
-
+    ++amount;
    }
    return amount;
   }
@@ -4571,15 +4566,10 @@ namespace BIRD2D
   unsigned int Tilemap::get_column_amount(const unsigned int viewport_height) const
   {
    unsigned int amount;
-   amount=0;
-   if (viewport_height>0)
+   amount=viewport_height/cell_height;
+   if ((viewport_height%cell_height)!=0)
    {
-    amount=viewport_height/cell_height;
-    if ((viewport_height%cell_height)!=0)
-    {
-     ++amount;
-    }
-
+    ++amount;
    }
    return amount;
   }
